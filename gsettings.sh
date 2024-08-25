13a6f352d6640aaaa1faa064217b2d50adca6b25
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-08-25 23:42:28 +0530

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -6,3 +6,6 @@ gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Console.desktop', 'org.gnome.Nautilus.desktop', 'chromium.desktop', 'firefox.desktop', 'google-chrome.desktop', 'virt-manager.desktop']"
+
+# set background color
+gsettings set org.gnome.desktop.background primary-color '#000000'
