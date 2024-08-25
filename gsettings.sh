6cb0226c884b77b24060232cac85827ccd7f3d1b
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-08-25 20:37:14 +0530

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -1,12 +1,8 @@
-
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse'
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
-
 gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Console.desktop', 'org.gnome.Nautilus.desktop', 'chromium.desktop', 'firefox.desktop', 'google-chrome.desktop', 'virt-manager.desktop']"
-
-echo "Applied custom gsettings."
