980d53f9e6e99200097f3b7d6f1764bbf11ae8a3
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-06 19:29:55 +0530

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -1,11 +1,22 @@
+# set dark theme
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse'
+gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse' # for legacy apps
+
+# set icon theme
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+
+# disable file history
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+# enable tap to click
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# show battery percentage
 gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Console.desktop', 'org.gnome.Nautilus.desktop', 'chromium.desktop', 'firefox.desktop', 'google-chrome.desktop', 'virt-manager.desktop']"
+
+# set favorite apps
+gsettings set org.gnome.shell favorite-apps "['kitty.desktop', 'org.gnome.Nautilus.desktop', 'firefox.desktop', 'google-chrome.desktop']"
 
 # set background color
 gsettings set org.gnome.desktop.background primary-color '#000000'
